7930375de1dfb10401c01d3908eb18a2ed1f4d96
Lem0nRavioli <57152284+Lem0nRavioli@users.noreply.github.com>
2021-10-01 16:09:53 +0200

    Add files via upload

---
+++ Dev/C/Exos/Pendu/pendu.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+// clear console
+void clrscr()
+{
+    system("@cls||clear");
+}
+
+void tour(int *life, char *word, char *show, char *bad, int len){
+    clrscr();
+    printf("You have %d life\n", *life);
+    printf("You already tried: %s\n", bad);
+    printf("%s\n", show);
+    char prop;
+    printf("Propose a letter: ");
+    scanf(" %c", &prop);
+    bool okmove = false;
+    for (int i=1;i<len - 1;i++) {
+        if (word[i] == prop) {
+            show[i] = prop;
+            okmove = true;
+        }
+    }
+    if (!okmove) {
+        printf("here !");
+        printf("%d\n", okmove);
+        *life -= 1;
+        strncat(bad, &prop, 1);
+    }
+}
+
+int main() {
+    char word[255], show[255], bad[255];
+    int life=8;
+    printf("Enter a word: ");
+    scanf("%s", word);
+    int len = strlen(word);
+    show[0] = word[0];
+    for (int i=1;i<len-1;i++) show[i] = '_';
+    show[len - 1] = word[len - 1];
+    show[len] = '\0';
+    while (life > 0) {
+        tour(&life, word, show, bad, len);
+        if (strcmp(word, show) == 0) break;
+    }
+    if (life == 0) printf("U suck lol");
+    else printf("Congratulation ! the word was: %s", show);
+
+}
