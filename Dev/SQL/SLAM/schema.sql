eddc2314be89a788d0440b9b2e7027011be32838
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-12-02 12:24:15 +0100

    update sql slam 12h24

--- Dev/SQL/SLAM/schema.sql
+++ Dev/SQL/SLAM/schema.sql
@@ -13,4 +13,18 @@ CREATE OR REPLACE TABLE USER (
 
 INSERT INTO USER VALUES (null, 'test', 'testpsw', 50);
 INSERT INTO USER VALUES (null, 'test', 'testpsw3', 40);
-INSERT INTO USER (EMAIL, PSW, TARIF) VALUES ('test2', 'testpsw', 70);
+-- autre syntaxe avec column name
+INSERT INTO USER (EMAIL, PSW, TARIF) VALUES ('test2', 'testpsw1', 70);
+INSERT INTO USER (EMAIL, PSW, TARIF) VALUES ('test5', 'testpsw2', 30);
+INSERT INTO USER (EMAIL, PSW, TARIF) VALUES ('test_', 'testpsw3', 180);
+INSERT INTO USER (EMAIL, PSW, TARIF) VALUES ('test_3', 'testpsw4', 10);
+INSERT INTO USER (EMAIL, PSW, TARIF) VALUES ('test0', 'testpsw65', 42);
+-- insert multiples avec column name
+INSERT INTO USER (EMAIL, PSW, TARIF)
+    VALUES
+    ('test5', 'passwood', 89),
+    ('test12', 'passwood3', 829),
+    ('test3', 'passwood2', 189),
+    ('test44', 'wordprass', 839),
+    ('test12', 'passwall', 389),
+    ('test1', 'passwoody', 1289);
