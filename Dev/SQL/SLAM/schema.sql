cd09f3edaf28a13c85a00d000049a8fdade89ec9
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-12-02 11:38:22 +0100

    update 11h38

--- Dev/SQL/SLAM/schema.sql
+++ Dev/SQL/SLAM/schema.sql
@@ -5,10 +5,12 @@ CREATE OR REPLACE TABLE USER (
     ID INT NOT NULL AUTO_INCREMENT , 
     EMAIL VARCHAR (255) NOT NULL,
     PSW VARCHAR (255) NOT NULL,
+    TARIF INT NOT NULL,
     -- 2 methods for primary key
     -- CONSTRAINT pk_user PRIMARY KEY (ID)
     PRIMARY KEY (ID)
 );
 
-INSERT INTO USER VALUES (null, 'test', 'testpsw');
-INSERT INTO USER (EMAIL, PSW) VALUES ('test2', 'testpsw');
+INSERT INTO USER VALUES (null, 'test', 'testpsw', 50);
+INSERT INTO USER VALUES (null, 'test', 'testpsw3', 40);
+INSERT INTO USER (EMAIL, PSW, TARIF) VALUES ('test2', 'testpsw', 70);
